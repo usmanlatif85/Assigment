ad800ddac6d7d6040be8175f585a1a02e4811110
usmanlatif85 <u208365@gmail.com>
2024-12-15 21:23:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+/*var a=2
+var b=++a *2
+
+console.log("anwser 1:", b)
+var x=5
+var y=x-- +2
+console.log("anwser 2:", y)
+var x=3
+var y=++x + x++ + ++x
+console.log("anwser 3:", y)
+var m=2
+var n=++m * m++ * --m
+console.log("anwser 4:", n)
+var a=3
+var b=5
+var result=++a +b-- -a++ + --b
+console.log("anwser 5:", result)
+var m=2
+var n=4
+var p=m++ + ++n - --m + n--
+console.log("anwser 6:","anwser m:", m,"anwser n:",n"anwser p:",p)
+var a=5
+var b=3
+var c=2
+var d=7
+var result=++a * (b-- + c)/ --d
+console.log("anwser 7:","anwser a:", a, "anwser b:",b,"anwser c:", c,"anwser d:", d)
+var m=2
+var n=3
+var o=4
+var result=m++ * (--n + m)/(o-- - n)
+console.log("anwser 8:","anwser m:", m"anwser n:", n,"anwser o:",  o)*/
